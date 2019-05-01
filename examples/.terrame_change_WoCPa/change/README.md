7f3c4ec800c847a3e6191594011f61e0c798ec77
bermr <marottabernardo@gmail.com>
2019-05-01 12:59:31 -0300

    remove unused files

--- examples/.terrame_change_WoCPa/change/README.md
+++
@@ -1,1 +0,0 @@
-# change visualization package
--- examples/.terrame_change_WoCPa/change/description.lua
+++
@@ -1,9 +0,0 @@
-version = "0.1"
-package = "change-visualization"
-license = "GPL"
-title = "Change Visualization Functions"
-depends = "terrame (>= 2.0)"
-authors = "Bernardo M. de Rezende, Tiago, G. S. Carneiro, Pedro R. Andrade"
-contact = "marottabernardo@gmail.com"
-content = "Implements functions where the change happening in the process can be visualized in maps as attributes"
-date = "01 maio 2019" -- automatically added by build
